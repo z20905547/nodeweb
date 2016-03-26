9fbda92395e6a842fa827c4a722f47004a8cc2cb
mjx831220 <153166355@qq.com>
2016-03-26 11:47:22 +0800

    按片区查询

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,17 @@ router.get('/', function(req, res, next) {
 	
   res.render('index', { title: '唯房会',backproId: 460000});
 });
+
+router.post('/findListBybname', function (req, res) {
+  res.render('index', { title: '唯房会',buildings_name:req.body.buildings_name,backproId: 460000});
+
+});
+router.get('/cityId/:city_id(\\d+)', function (req, res) {
+  res.render('index', { title: '唯房会',city_id:req.params.city_id,backproId: 460000});
+});
+
+router.get('/areaId/:area_id(\\d+)', function (req, res) {
+  res.render('index', { title: '唯房会',area_id:req.params.area_id,backproId: 460000});
+});
+
 module.exports = router;
