73e6cdccf5bed3646367ff5db5272b0638ff564e
mjx831220 <153166355@qq.com>
2016-04-12 12:18:23 +0800

    百度搜索优化

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 router.get("/:proId(\\d+)", function(req, res, next) {
 	//获取楼盘数据
-  res.render('index', { title: '唯房会',backproId: req.params.proId});
+  res.render('index', { title: '唯房会 -一家做特价房的网站',backproId: req.params.proId});
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	//获取楼盘数据
 	
-  res.render('index', { title: '唯房会',backproId: 460000});
+  res.render('index', { title: '唯房会 -一家做特价房的网站',backproId: 460000});
 });
 
 router.post('/findListBybname', function (req, res) {
-  res.render('index', { title: '唯房会',buildings_name:req.body.buildings_name,backproId: 460000});
+  res.render('index', { title: '唯房会 -一家做特价房的网站',buildings_name:req.body.buildings_name,backproId: 460000});
 
 });
 router.get('/cityId/:city_id(\\d+)', function (req, res) {
-  res.render('index', { title: '唯房会',city_id:req.params.city_id,backproId: 460000});
+  res.render('index', { title: '唯房会 -一家做特价房的网站',city_id:req.params.city_id,backproId: 460000});
 });
 
 router.get('/areaId/:area_id(\\d+)', function (req, res) {
-  res.render('index', { title: '唯房会',area_id:req.params.area_id,backproId: 460000});
+  res.render('index', { title: '唯房会 -一家做特价房的网站',area_id:req.params.area_id,backproId: 460000});
 });
 
 module.exports = router;
