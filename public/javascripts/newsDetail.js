a8025809d2d0ee7beecb3e1aeb3be8e8bd6f92b5
mujiaxing4 <mail.126.com>
2019-01-02 17:16:17 +0800

    首页头条调整  文章图片自适应调整

--- public/javascripts/newsDetail.js
+++ public/javascripts/newsDetail.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 				'	         <p class="p2"> <span class="time"><edit type="text" name="Content_RightContentArtReleaseDate"></edit><time>发布日期：'+data.data.MessageDate+'</time></span></p>'+
 				'			  </div>'+
 				'             <big><img class="J_art_content_p" src="http://www.vfhui.com:8080/management/resource/upload_buildings/news/'+data.data.Id+'/fm.jpg" onerror="javascript:this.style.display=\'none\'"'+'></big>'+
-				'			  <div id="newsContent" style="width:100%;overflow:hidden;font-size: 16px;" class="text" deep="2">	<p style="text-indent:32px;text-autospace:ideograph-numeric;line-height:29px"><span style=";font-family:微软雅黑;font-size:16px">'+data.data.Content +'</span></p>  </div>';
+				'			  <div id="newsContent" style="width:100%;height:auto;overflow:hidden;font-size: 16px;">	<p style="text-indent:32px;text-autospace:ideograph-numeric;line-height:29px"><span style=";font-family:微软雅黑;font-size:16px">'+data.data.Content +'</span></p>  </div>';
 			$(".box_show02").html(adcontent1);
 			$(".border-bottom").html(adcontent1);
 			$(".J_last").html('&gt;&gt;'+data.data.Title);
